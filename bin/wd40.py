6b2baf8fafcf73a8f864ca0eb3be32c0013ecb56
WardDeb <ward@deboutte.be>
2020-12-14 10:42:57 +0100

    Cat projects

--- bin/wd40.py
+++ bin/wd40.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 import argparse
 import wdforty.misc
+import wdforty.catRun
 import textwrap
+import sys
 
 parser = argparse.ArgumentParser(description='Here to make your life easy! fac.py <command> [<args>]')
-parser.add_argument('command', type=str, choices = ['storageHammer', 'projCP', 'fastQC'],
+parser.add_argument('command', type=str, choices = ['storageHammer', 'projCP', 'fastQC', 'catRun'],
                     help='Give the command you would like to perform. ')
+parser.add_argument('--projects', 
+                    help='catRun: list the projects you would like to combine. Seperated by comma!')
+parser.add_argument('--flowcells',
+                    help='catRun: list the flowcells (looked for under baseDir specified in ini) where the projects reside.')
 args = parser.parse_args()
 
-config = Facilitator.misc.getConfig()
-if args.Command == 'projCP':
-    Facilitator.misc.projCP(config['projCP']['destination'])
+config = wdforty.misc.getConfig()
+if args.command == 'projCP':
+    wdforty.misc.projCP(config['projCP']['destination'])
 
-if args.Command == 'storageHammer':
+if args.command == 'storageHammer':
     PIs = config['storageHammer']['PIs'].split(',')
     prefix = config['storageHammer']['prefix']
     postfix = config['storageHammer']['postfix']
-    Facilitator.misc.storageHammer(PIs, prefix, postfix)
+    wdforty.misc.storageHammer(PIs, prefix, postfix)
 
+if args.command == 'catRun':
+    try:
+        Projects = args.projects.replace(' ','').split(',')
+    except:
+        print("catRun module requires projects specified with --project Proj1,Proj2")
+        sys.exit()
+    try:
+        Flowcells = args.flowcells.replace(' ','').split(',')   
+    except:
+        print("catRun module requires flowcells specified with --flowcells flow1,flow2")
+        sys.exit()
+    baseDir = config['catRun']['baseDir']
+    wdforty.catRun.catRun(Projects, Flowcells, baseDir)
+    
---
+++ wdforty/catRun.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+def catRun(Projects, flowcells, baseDir):
+    for proj in Projects:
+        os.mkdir(proj)
+        Samples = [sample for sample in os.listdir(os.path.join(baseDir,flowcells[0],proj)) if 'Sample_' in sample]
+        print("found {} Samples ({})".format(len(Samples), ','.join(Samples)))
+        for sample in Samples:
+            catOutDir = os.path.join(proj, sample)
+            os.mkdir(catOutDir)
+            # Get all R1s
+            R1s = []
+            R2s = []
+            for flow in flowcells:
+                R1 = [R1fq for R1fq in os.listdir(os.path.join(baseDir, flow, proj, sample)) if 'R1.fastq.gz' in R1fq]
+                R1s.append(os.path.join(baseDir,flow, proj, sample,R1[0]))
+                R2 = [R2fq for R2fq in os.listdir(os.path.join(baseDir, flow, proj, sample)) if 'R2.fastq.gz' in R2fq]
+                R2s.append(os.path.join(baseDir,flow, proj, sample,R2[0]))
+            if len(R1s) != len(flowcells):
+                print("Wow, seems like there are samples missing, I'm stopping.")
+                break
+            #Catting time.
+            catName = R1s[0].split('/')[-1]
+            catcmdR1 = ['cat'] + R1s
+            with open(os.path.join(catOutDir, catName), "w") as out1:
+                subprocess.run(catcmdR1, stdout=out1)
+            catName = R2s[0].split('/')[-1]
+            catcmdR2 = ['cat'] + R2s
+            with open(os.path.join(catOutDir, catName), "w") as out2:
+                subprocess.run(catcmdR2, stdout=out2)
+    return "Done."
