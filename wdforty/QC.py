712899007149c79e00c4d732d76007f1d5e697d6
WardDeb <ward@deboutte.be>
2021-02-18 16:20:02 +0100

    CleanQC

--- wdforty/QC.py
+++ wdforty/QC.py
@@ -1,37 +1,26 @@
 import os
 import subprocess
+import glob
 
 def fastQCrunner(fastQC, multiQC):
     projectDirs = []
     for i in os.listdir():
         if 'Project_' in i and 'FASTQC' not in i:
             projectDirs.append(i)
-    if len(projectDirs) == 0:
-        print('I could not find any project directories.')
-    else:
-        print("Can't find a project directory.")
+            print("Project {} found.".format(i))
     for projectDir in projectDirs:
         QCpath = "FASTQC_" + projectDir
         if os.path.isdir(QCpath):
             print("{} already exists. Please remove it yourself.".format(QCpath))
         else:
-            sampleNames = []
-            for i in os.listdir(projectDir):
-                if 'Sample' in i:
-                    sampleNames.append(i)
+            fqFiles = [i for i in glob.glob(projectDir + "/*/*fastq.gz") if 'optical' not in i]
             os.mkdir(QCpath)
-            for sample in sampleNames:
-                fastQCout = os.path.join(QCpath, sample)
-                readFiles = []
-                for i in os.listdir(os.path.join(projectDir, sample)):
-                    if 'fastq.gz' in i and 'optical' not in i:
-                        readFiles.append(os.path.join(projectDir, sample, i))         
-                os.mkdir(fastQCout)
-                fastQCcmd = [fastQC,"-t", "20", "-o",fastQCout,readFiles[0],readFiles[1]]
-                subprocess.run(fastQCcmd)
+            for item in fqFiles:
+                print(item)
+            fastQCcmd = [fastQC,"-t", "30", "-o",QCpath] + fqFiles
+            print(' '.join(fastQCcmd))
+            subprocess.run(fastQCcmd)
             #run multiQC
-        if not os.path.exists(os.path.join(projectDir, 'multiqc_report.html')):
-            multiQCcmd = [multiQC, '-o',projectDir, QCpath]
-            subprocess.run(multiQCcmd)
-
-
+            if not os.path.exists(os.path.join(projectDir, 'multiqc_report.html')):
+                multiQCcmd = [multiQC, '-o',projectDir, QCpath]
+                subprocess.run(multiQCcmd)
